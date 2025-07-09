6ba4a427bb8f25330fd3ed88e35589c021943c32
XuhanYang888 <xuhany@protonmail.com>
2025-07-09 16:24:01 -0400

    finish contact form

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-// smooth scroll
-document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-    const target = document.querySelector(this.getAttribute("href"));
-    if (target) {
-      target.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-      });
-    }
-  });
-});
+console.log("Script loaded!");
 
 // scroll effect for header
 window.addEventListener("scroll", () => {
@@ -59,4 +47,50 @@ const observer = new IntersectionObserver((entries) => {
 const statsSection = document.querySelector(".stats");
 if (statsSection) {
   observer.observe(statsSection);
+}
+
+// contact form submission
+const form = document.getElementById("form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const object = Object.fromEntries(formData);
+  const json = JSON.stringify(object);
+
+  fetch("https://api.web3forms.com/submit", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: json,
+  })
+    .then(async (response) => {
+      let json = await response.json();
+      if (response.status == 200) {
+        showToast(json.message);
+      } else {
+        console.log(response);
+        showToast(json.message);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      showToast("Something went wrong!");
+    })
+    .then(function () {
+      form.reset();
+    });
+});
+
+function showToast(message) {
+  const toast = document.getElementById("toast");
+  toast.innerHTML = "";
+  toast.textContent = message;
+  toast.classList.add("show");
+
+  setTimeout(() => {
+    toast.classList.remove("show");
+  }, 4000);
 }
